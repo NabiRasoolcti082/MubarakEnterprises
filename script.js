b18d2a7708a1c8f9924169ea1f13e3a7208e7cea
NabiRasoolcti082 <119929029+NabiRasoolcti082@users.noreply.github.com>
2023-07-06 23:48:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("slideshow-container")[0].getElementsByTagName("img");
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+      }
+      slides[slideIndex-1].style.opacity = 1;
+    }
+    const purchaseButton = document.querySelector('.purchase-button');
+
+purchaseButton.addEventListener('click', () => {
+  // Handle the purchase action here
+});
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
